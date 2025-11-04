a9be603abd2966dc2ad57291de2ba95c9db00ac5
Dhaval Shah <44603496+dhaval512@users.noreply.github.com>
2025-11-04 12:37:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ PORT = int(os.getenv("PORT", "8080"))
 
 @app.route("/hello")
 def hello():
-    return f"Hello from {APP_NAME}!"
+    return f"Hello from /hello path {APP_NAME}!"
 
 @app.route("/about")
 def about():
-    return f"{APP_NAME}: a simple demo app."
+    return f"{APP_NAME}: a simple demo app from /about path."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
